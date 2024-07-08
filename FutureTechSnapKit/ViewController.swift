50458d5e6fd5781db3da3677a8263b86852b007d
Omer Keskin <keskinomerf@gmail.com>
2024-07-08 10:25:50 +0300

    Initial Commit

---
+++ FutureTechSnapKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FutureTechSnapKit
+//
+//  Created by Omer Keskin on 8.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
